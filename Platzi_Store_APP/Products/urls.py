d2bf02fad47b645ed154e19a953287387aee1f32
Alejandra Camargo <avelandia386@gmail.com>
2025-09-04 08:22:46 -0500

    Actualizamos las urls para agregar los endpoints de update y delete

--- Platzi_Store_APP/Products/urls.py
+++ Platzi_Store_APP/Products/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('products/', views.consult_products, name='consult_products'),
     path('products/create/', views.create_product, name='create_product'),
     path('products/<int:pk>/', views.detail_product, name='detail_product'),
+    path('products/<int:pk>/update/', views.update_product, name='update_product'),
+    path('products/<int:pk>/delete/', views.delete_product, name='delete_product'),
 ]
